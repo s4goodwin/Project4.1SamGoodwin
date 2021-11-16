f8f129fa4035f5e37e640d5a8112b0de7179ea29
s4goodwin <90581608+s4goodwin@users.noreply.github.com>
2021-11-15 23:10:33 -0500

    added textfield, price type and namefield

--- src/main/java/bsu/comp152/project4_1/HelloController.java
+++ src/main/java/bsu/comp152/project4_1/HelloController.java
@@ -3,6 +3,7 @@ package bsu.comp152.project4_1;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,6 +11,13 @@ import java.util.ResourceBundle;
 public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private TextField PriceField;
+    @FXML
+    private TextField TypeField;
+    @FXML
+    private TextField NameField;
 
     @FXML
     protected void onHelloButtonClick() {
